cad3d80f7cd67671f4f7d0bb99488e1d83b5fb35
Measured Moth <96396280+MeasuredMoth@users.noreply.github.com>
2025-01-26 11:43:06 -0800

    Added bulk egg2bam script

---
+++ bulkegg2bam.ps1
@@ -0,0 +1,13 @@
+$eggfiles = @(Get-ChildItem -Path * -Filter *.egg)
+
+Write-Host 'Converting .egg to .bam...'
+
+for ($i = 0 ; $i -le $eggfiles.Length ; $i++){
+    #building the array
+    Write-Host 'Found you '$eggfiles[$i]
+    $bamname = $eggfiles[$i] -Replace '\.egg$','.bam'
+    Write-Host 'Should come out to '$eggfiles[$i]
+    Write-Host '
+    '
+    .\egg2bam.exe $eggfiles[$j].toString() $bamname.ToString()
+}
